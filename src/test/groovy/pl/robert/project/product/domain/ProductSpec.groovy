22b8277b21ade31192a647fd107eee17b5aa9f75
RoberKrzywina <robert.krzywina@gmail.com>
2019-05-13 18:25:58 +0200

    Add scenario of a test

---
+++ src/test/groovy/pl/robert/project/product/domain/ProductSpec.groovy
@@ -0,0 +1,12 @@
+package pl.robert.project.product.domain
+
+import spock.lang.Specification
+
+class ProductSpec extends Specification {
+
+    def 'Should add product'() {
+        when: 'we add a product'
+
+        then: 'system has this product'
+    }
+}
