39f3d13c0fabe1d5c59608d05e33cc22f13fe183
RoberKrzywina <robert.krzywina@gmail.com>
2019-06-04 20:44:17 +0200

    Display list of items

--- src/main/java/pl/robert/project/ProjectApplication.java
+++ src/main/java/pl/robert/project/ProjectApplication.java
@@ -1,13 +1,34 @@
 package pl.robert.project;
 
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import pl.robert.project.server.product.domain.ProductFacade;
+import pl.robert.project.server.product.domain.dto.CreateProductDto;
 
 @SpringBootApplication
 public class ProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
-    }
+        ApplicationContext context = SpringApplication.run(ProjectApplication.class);
 
+        ProductFacade facade = context.getBean(ProductFacade.class);
+
+        Stream.of(
+                new CreateProductDto(1L, "Apple"),
+                new CreateProductDto(2L, "Banana"),
+                new CreateProductDto(3L, "Fruit"),
+                new CreateProductDto(4L, "Orange"),
+                new CreateProductDto(5L, "Grape"),
+                new CreateProductDto(6L, "Lemon"),
+                new CreateProductDto(7L, "Mango"),
+                new CreateProductDto(8L, "Pear"),
+                new CreateProductDto(9L, "Coconut"),
+                new CreateProductDto(10L, "Plum"),
+                new CreateProductDto(11L, "Watermelon")
+        ).forEach(facade::create);
+    }
 }
--- src/main/java/pl/robert/project/client/Client.java
+++ src/main/java/pl/robert/project/client/Client.java
@@ -1,17 +1,54 @@
 package pl.robert.project.client;
 
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Route(value = "")
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import org.springframework.data.domain.PageRequest;
+
+import pl.robert.project.server.product.domain.ProductFacade;
+import pl.robert.project.server.product.domain.dto.ProductDto;
+
+@Route("")
+@Theme(Lumo.class)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Client extends VerticalLayout {
 
-    public Client() {
-        VerticalLayout layout = new VerticalLayout();
+    ProductFacade facade;
 
-        layout.add(new Label("Hello world"));
+    public Client(ProductFacade facade) {
+        this.facade = facade;
 
-        add(layout);
+        addHeader();
+        addContent();
+    }
+
+    private void addHeader() {
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        add(new Label("Product management"));
+    }
+
+    private void addContent() {
+        List<ProductDto> products = facade.readAll(PageRequest.of(0, 10))
+                .get()
+                .collect(Collectors.toList());
+
+        Grid<ProductDto> grid = new Grid<>();
+
+        grid.setItems(products);
+
+        grid.addColumn(ProductDto::getId).setHeader("Id");
+        grid.addColumn(ProductDto::getName).setHeader("Name");
+
+        add(grid);
     }
 }
